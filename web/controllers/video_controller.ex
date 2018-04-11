bf95607a86866b0bb09caa9f21dd376b04bc2272
Christopher Eyre <christopher.eyre@team.pottermore.com>
2018-04-11 16:43:59 +0100

    Finished Chapter 7.

--- web/controllers/video_controller.ex
+++ web/controllers/video_controller.ex
@@ -2,6 +2,9 @@ defmodule Rumbl.VideoController do
   use Rumbl.Web, :controller
 
   alias Rumbl.Video
+  alias Rumbl.Category
+
+  plug :load_categories when action in [:new, :create, :edit, :update]
 
   #This rewrites the default action method to add a user param to all methods
   def action(conn, _) do
@@ -79,5 +82,14 @@ defmodule Rumbl.VideoController do
 
   defp user_videos(user) do
     assoc(user, :videos)
+  end
+
+  defp load_categories(conn, _) do
+    query =
+       Category
+       |> Category.alphabetical
+       |> Category.names_and_ids
+    categories = Repo.all query
+    assign(conn, :categories, categories)   
   end
 end
